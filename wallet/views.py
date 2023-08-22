e98cfccd01bee2df133df22d5cafb5a172b79df3
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-08-23 00:48:14 +0100

    Deposit endpoint

--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,7 @@
+import datetime
+import uuid
+from uuid import UUID
+
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 
@@ -18,6 +22,29 @@ class WalletViewSet(ModelViewSet):
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
 
+    @action(detail=True, methods=['post'])
+    def deposit(self, sender_wallet_number, amount, receiver_wallet_number):
+        sender = Wallet.objects.get(pk=sender_wallet_number)
+        receiver = Wallet.objects.get(pk=receiver_wallet_number)
+
+        transaction = Transaction.objects.create()
+        transaction.type = 'DEPOSIT'
+        transaction.date_time = datetime.datetime.now()
+        transaction.amount = amount
+        transaction.wallet = sender
+        transaction.reference_number = uuid.uuid4()
+
+        updated_balance = sender.balance
+
+        if amount > 0:
+            updated_balance = sender.balance + amount
+
+        if sender.balance != updated_balance:
+            sender.balance += updated_balance
+            transaction.status = 'SUCCESSFUL'
+            transaction.save()
+            sender.save()
+
     # @action(detail=True, methods=['post'])
     # def set_password(self, request, pk=None):
     #     user = self.get_object()
@@ -30,10 +57,10 @@ class WalletViewSet(ModelViewSet):
     #         return Response(serializer.errors,
     #                         status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=True, methods=['post'])
-    def deposit(self, request, pk=None):
-        wallet = self.get_object()
-        # serializer =
+    # @action(detail=True, methods=['post'])
+    # def deposit(self, request, pk=None):
+    #     wallet = self.get_object()
+    # serializer =
 
 
 class TransactionViewSet(ModelViewSet):
